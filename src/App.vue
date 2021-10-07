56c1f6d1ab2aa9dbf875610a8901bd1ca73ddb45
Breno Ferreira Rodrigues <89168810+Breno7K@users.noreply.github.com>
2021-10-07 19:41:47 -0400

    Add files via upload
     - Cadastro de médico;
    - Partes não finalizadas.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template >
+
+  <div id="app">
+    
+    <addmedico></addmedico>
+  
+
+  </div>
+
+</template>
+
+<script>
+import addmedico from './components/med-cadastro';
+
+export default{
+  name: 'app',
+  components: {
+    addmedico,
+  }
+}
+
+</script>
+
+<style>
+
+
+</style>
